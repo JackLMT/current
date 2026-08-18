5d826af07b2e4f314ad58e0be23c03d24c23e841
JackLMT <mohammademamgoli@gmail.com>
2026-08-18 16:15:57 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,27 @@
 
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      success: false,
+      message: "Only POST requests are allowed"
+    });
+  }
+
+  try {
+    const data = req.body;
+
+    console.log("Received from SIM800L:", data);
+
+    return res.status(200).json({
+      success: true,
+      message: "Data received",
+      data: data
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Server error"
+    });
+  }
+}
